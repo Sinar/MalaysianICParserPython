f96dc2db03e2808c8a8b345b5f0fc4389d5acb56
sweemeng <sweester@gmail.com>
2011-08-29 14:28:17 +0800

    split process into multiple method

--- nric_parser.py
+++ nric_parser.py
@@ -9,14 +9,18 @@ class ICParser:
         ic_digits_only = r'\d{12}'
         ic_split_pattern = r'(\d{6})(\d{2})(\d{4})'
         if re.match(ic_digits_only,ic):
-            ic_token = re.split(ic_split_pattern,ic)
-            ic_token = [i for i in ic_token if i]
+            self.ic_token = re.split(ic_split_pattern,ic)
+            self.ic_token = [i for i in self.ic_token if i]
         elif re.match(ic_with_dash,ic):
-            ic_token = ic.split('-')
+            self.ic_token = ic.split('-')
         else:
             raise InvalidFormatException(ic)
 
-        self.birth_date = parse(ic_token[0]).date()
+        self.set_gender()
+        self.set_birth_date()
+
+    def set_birth_date(self):
+        self.birth_date = parse(self.ic_token[0]).date()
         if self.birth_date > datetime.date.today():
             raise InvalidDateException(self.birth_date)
 
@@ -25,13 +29,14 @@ class ICParser:
         
         if date_diff.years < 12:
             raise InvalidDateException(self.birth_date)
-        
-        last_no = ic_token[2][-1]
+
+    def set_gender(self):
+        last_no = self.ic_token[2][-1]
         if int(last_no) % 2:
             self.gender = 'M'
         else:
             self.gender = 'F'
-
+      
 class InvalidFormatException(Exception):
     def __init__(self,value):
         self.value = value
