2a77ee68f7318bc71dcc95b81b4b40d25cfdb1db
sweemeng <sweester@gmail.com>
2011-08-29 13:34:21 +0800

    add implementation needed for imports

--- nric_parser.py
+++ nric_parser.py
@@ -16,3 +16,12 @@ class InvalidDateException(Exception):
     
     def __str__(self):
         return repr(self.value)
+
+
+class InvalidBirthPlace(Exception):
+    def __init__(self,value):
+        self.value = value
+    
+    def __str__(self):
+        return repr(self.value)
+   
