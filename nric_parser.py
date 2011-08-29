a38dc74ffdbf883d41d25fd083f6d02281be1af1
sweemeng <sweester@gmail.com>
2011-08-29 13:31:15 +0800

    initial implementation of InvalidFormatException

---
+++ nric_parser.py
@@ -0,0 +1,10 @@
+class ICParser:
+    def __init__(self,ic):
+        pass
+
+class InvalidFormatException(Exception):
+    def __init__(self,value):
+        self.value = value
+    
+    def __str__(self):
+        return repr(self.value)
