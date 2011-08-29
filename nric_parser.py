4136ad57f15506530a01922092275d84725923fd
sweemeng <sweester@gmail.com>
2011-08-29 13:32:36 +0800

    invalid date exception initial implementation

--- nric_parser.py
+++ nric_parser.py
@@ -8,3 +8,11 @@ class InvalidFormatException(Exception):
     
     def __str__(self):
         return repr(self.value)
+
+
+class InvalidDateException(Exception):
+    def __init__(self,value):
+        self.value = value
+    
+    def __str__(self):
+        return repr(self.value)
